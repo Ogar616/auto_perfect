66719aa5c4f884c662311b471d32c50da48b13f0
Ogar616 <kamil.sobczyk@wp.pl>
2018-09-02 11:55:10 +0200

    content added, about modified, parials added

--- src/js/app.js
+++ src/js/app.js
@@ -10,7 +10,7 @@ const scrollTo = (element) => {
 
 document.addEventListener("DOMContentLoaded", () => {
 
-    console.log("DOM LOADED");
+    //scrolling page to sections
     const anchors = document.querySelectorAll("a");
 
     anchors[0].addEventListener("click", () => {
@@ -22,7 +22,7 @@ document.addEventListener("DOMContentLoaded", () => {
     });
 
     anchors[2].addEventListener("click", () => {
-        scrollTo(document.getElementById("aboutgallery"));
+        scrollTo(document.getElementById("gallery"));
     });
 
     anchors[3].addEventListener("click", () => {
@@ -33,14 +33,15 @@ document.addEventListener("DOMContentLoaded", () => {
         scrollTo(document.getElementById("contact"));
     });
 
-    /*------------------------------
-        Album Cover Slider
---------------------------------*/
-//start added by Chase
-    var a = document.getElementsByTagName("a");
-    var cfImg = document.getElementsByClassName("coverflow__image")
+    //slider
+    const prevDef = (e) => {
+        e.preventDefault();
+    };
 
-    var scaleI = 0;
+    const a = document.getElementsByTagName("a");
+    const cfImg = document.getElementsByClassName("coverflow__image")
+
+    let scaleI = 0;
     for (scaleI; scaleI < a.length; scaleI++) {
         if (scaleI === 3) {
             continue;
@@ -50,11 +51,9 @@ document.addEventListener("DOMContentLoaded", () => {
         }
     }
 
-    function prevDef(e) {
-        e.preventDefault();
-    }
 
-    function forScale(coverflowPos) {
+
+    const forScale = (coverflowPos) => {
         for (scaleI = 0; scaleI < a.length; scaleI++) {
             a[scaleI].style.cursor = "default";
             a[scaleI].addEventListener("click", prevDef);
@@ -65,11 +64,10 @@ document.addEventListener("DOMContentLoaded", () => {
                 cfImg[scaleI].parentElement.removeEventListener("click", prevDef);
             }
         }
-    }
-//end added by Chase
+    };
 
-    function setupCoverflow(coverflowContainer) {
-        var coverflowContainers;
+    const setupCoverflow = (coverflowContainer) => {
+        let coverflowContainers;
 
         if (typeof coverflowContainer !== "undefined") {
             if (Array.isArray(coverflowContainer)) {
@@ -81,74 +79,68 @@ document.addEventListener("DOMContentLoaded", () => {
             coverflowContainers = Array.prototype.slice.apply(document.getElementsByClassName('coverflow'));
         }
 
-        coverflowContainers.forEach(function(containerElement) {
-            var coverflow = {};
-            var prevArrows, nextArrows;
+        coverflowContainers.forEach(containerElement => {
+            let coverflow = {};
+            let prevArrows, nextArrows;
 
-            //capture coverflow elements
             coverflow.container = containerElement;
             coverflow.images = Array.prototype.slice.apply(containerElement.getElementsByClassName('coverflow__image'));
             coverflow.position = Math.floor(coverflow.images.length / 2) + 1;
 
-            //set indicies on images
-            coverflow.images.forEach(function(coverflowImage, i) {
+            coverflow.images.forEach((coverflowImage, i) => {
                 coverflowImage.dataset.coverflowIndex = i + 1;
             });
 
-            //set initial position
             coverflow.container.dataset.coverflowPosition = coverflow.position;
 
-            //get prev/next arrows
             prevArrows = Array.prototype.slice.apply(coverflow.container.getElementsByClassName("prev-arrow"));
             nextArrows = Array.prototype.slice.apply(coverflow.container.getElementsByClassName("next-arrow"));
 
-            //add event handlers
-            function setPrevImage() {
+            const setPrevImage = () => {
                 coverflow.position = Math.max(1, coverflow.position - 1);
                 coverflow.container.dataset.coverflowPosition = coverflow.position;
-                //call the functin forScale added
                 forScale(coverflow.position);
-            }
+            };
 
-            function setNextImage() {
+
+            const setNextImage = () => {
                 coverflow.position = Math.min(coverflow.images.length, coverflow.position + 1);
                 coverflow.container.dataset.coverflowPosition = coverflow.position;
-                //call the function Chase added
                 forScale(coverflow.position);
-            }
+            };
 
-            function jumpToImage(evt) {
+            const jumpToImage = evt => {
                 coverflow.position = Math.min(coverflow.images.length, Math.max(1, evt.target.dataset.coverflowIndex));
                 coverflow.container.dataset.coverflowPosition = coverflow.position;
-                //start added by Chase
-                setTimeout(function() {
+                setTimeout(() => {
                     forScale(coverflow.position);
                 }, 1);
-                //end added by Chase
-            }
+            };
 
-            function onKeyPress(evt) {
+            const onKeyPress = evt => {
                 switch (evt.which) {
-                    case 37: //left arrow
+                    case 37:
                         setPrevImage();
                         break;
-                    case 39: //right arrow
+                    case 39:
                         setNextImage();
                         break;
                 }
-            }
-            prevArrows.forEach(function(prevArrow) {
+            };
+
+            prevArrows.forEach(prevArrow => {
                 prevArrow.addEventListener('click', setPrevImage);
             });
-            nextArrows.forEach(function(nextArrow) {
+            nextArrows.forEach(nextArrow => {
                 nextArrow.addEventListener('click', setNextImage);
             });
-            coverflow.images.forEach(function(image) {
+            coverflow.images.forEach(image => {
                 image.addEventListener('click', jumpToImage);
             });
             window.addEventListener('keyup', onKeyPress);
         });
-    }
+    };
+
 
     setupCoverflow();
 });
